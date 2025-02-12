2d656b95c7ce889075435a664842329d0849ec17
Marco Rodríguez <marodriguezvar@unal.edu.co>
2025-02-11 23:59:22 -0500

    integracion

---
+++ SoftwareGanadero/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from config import SQLALCHEMY_DATABASE_URI
+from models import db
+from routes import routes
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
+CORS(app)
+
+app.register_blueprint(routes)
+
+if __name__ == "__main__":
+    app.run(debug=True)
