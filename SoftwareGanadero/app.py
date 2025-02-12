694db4debd7a3966e4c05fdc8421f76b2db29068
Marco Rodríguez <marodriguezvar@unal.edu.co>
2025-02-12 13:04:41 -0500

    method GET update

--- SoftwareGanadero/app.py
+++ SoftwareGanadero/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from config import SQLALCHEMY_DATABASE_URI
 from models import db
 from routes import routes
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-@app.route("/subir_imagen", methods=["POST"])
+@app.route("/subir_imagen", methods=["GET","POST"])
 def subir_imagen():
     return {"mensaje": "Ruta funcionando correctamente"}
 
