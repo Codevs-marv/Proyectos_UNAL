d0184f2417453b05603d19325e748a70db167a9b
Marco Rodríguez <marodriguezvar@unal.edu.co>
2025-02-19 19:39:14 -0500

    conectar bd insumos

--- SoftwareGanadero/config.py
+++ SoftwareGanadero/config.py
@@ -1,1 +1,2 @@
 SQLALCHEMY_DATABASE_URI = "mssql+pyodbc://@NITROMARV/InventarioAnimales?trusted_connection=yes&driver=SQL+Server"
+SQLALCHEMY_DATABASE_URI = "mssql+pyodbc://@NITROMARV/InventarioInsumos?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes"
